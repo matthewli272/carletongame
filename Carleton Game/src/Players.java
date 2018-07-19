1149a8df974ba9c6509859b4bbe37ff6b7411182
Chi <jchi19@carlguest-190-196.dyn.carleton.edu>
2018-07-19 14:05:47 -0500

    added players+weapons classes

---
+++ Carleton Game/src/Players.java
@@ -0,0 +1,41 @@
+public class Players {
+    private int playerHealth;
+    private Weapons currentWeapon;
+    private String name;
+    private int playerType;
+    private int playerX;
+    private int playerY;
+
+    public Players(String name, int playerType ){
+        this.name = name;
+        this.playerType = playerType;
+    }
+
+
+
+    //Getters & Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getPlayerX() {
+        return playerX;
+    }
+
+    public void setPlayerX(int playerX) {
+        this.playerX = playerX;
+    }
+
+    public int getPlayerY() {
+        return playerY;
+    }
+
+    public void setPlayerY(int playerY) {
+        this.playerY = playerY;
+    }
+}
---
+++ Carleton Game/src/Weapons.java
@@ -0,0 +1,15 @@
+public class Weapons {
+    private String wpnName;
+    private int wpnDmg;
+
+    public Weapons(String wpnName){
+        this.wpnName = wpnName;
+
+    }
+
+
+    //Getters & Setters
+    public int getWpnDmg() {
+        return wpnDmg;
+    }
+}
