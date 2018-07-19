95e4abb84a07032058dd62777f21f70fa493a4e5
Chi <jchi19@carlguest-190-196.dyn.carleton.edu>
2018-07-19 09:55:53 -0500

    added window creation

--- Carleton Game/src/main.java
+++ Carleton Game/src/main.java
@@ -1,9 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class main {
+public class Main extends JFrame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Main(double width, double height){
+
+		super("Carleton");
+
+
+
+		Container window = getContentPane();
+
 
 	}
 
+	public static void main(String[] args) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		int setWidth = (int) (width * (2.0 / 3.0));
+		int setHeight = (int) (height * (5.0 / 6.0));
+
+		Main myApp = new Main(setWidth,setHeight);
+		myApp.setBounds((int) (setWidth * (1.0 / 3.0)) ,50,setWidth ,setHeight );
+
+		myApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myApp.setResizable(false);
+		myApp.setVisible(true);
+	}
 }
